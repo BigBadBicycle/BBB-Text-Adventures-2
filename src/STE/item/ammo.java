3e7afdc44129369f49072d7426c4f835b06d0d4b
Santiago <santi200822@outlook.com>
2025-06-30 21:51:33 -0500

    ammo.

--- src/STE/item/ammo.java
+++ src/STE/item/ammo.java
@@ -1,4 +1,10 @@
 package STE.item;
 
-public class ammo {
+public class ammo extends item{
+
+    int amount;
+    ammo(String name, int valuable, boolean isEquipable,int amount) {
+        super(name, valuable, isEquipable);
+        this.amount = amount;
+    }
 }
